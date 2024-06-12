62a317a9ed73edac1544a09d65fd2bcd83688d04
Mayur Kalwar <mayurkalwar0251@gmail.com>
2024-06-12 15:52:48 +0530

    Added images

--- src/components/collaborating closely pink/CollaboratingClosely.jsx
+++ src/components/collaborating closely pink/CollaboratingClosely.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./collaborating.css";
 import Checkmark from "../../assets/checkmarx-logo-2.webp";
+import IBM from "../../assets/ibm-logo.png";
+import Micro from "../../assets/micro-foc.png";
+import IzoLogic from "../../assets/izoologic_logo.png";
 
 const CollaboratingClosely = () => {
   return (
@@ -14,21 +17,21 @@ const CollaboratingClosely = () => {
           <p className="collab-head-right">CyberSecurity Solutions</p>
         </div>
 
-        <div className="collab-card">
+        <div className="collab-card" data-aos="fade-right">
           <div className="card">
             <img src={Checkmark} alt="Checkmark" />
           </div>
           <div className="card">
-            <img src={Checkmark} alt="Checkmark" />
+            <img src={IBM} alt="Checkmark" />
           </div>
           <div className="card">
-            <img src={Checkmark} alt="Checkmark" />
+            <img src={Micro} alt="Checkmark" />
           </div>
           <div className="card">
-            <img src={Checkmark} alt="Checkmark" />
+            <img src={IzoLogic} alt="Checkmark" />
           </div>
           <div className="card">
-            <img src={Checkmark} alt="Checkmark" />
+            <img src={IBM} alt="Checkmark" />
           </div>
           <div className="card">
             <img src={Checkmark} alt="Checkmark" />
